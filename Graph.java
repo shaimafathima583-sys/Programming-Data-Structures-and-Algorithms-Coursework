2315e775fa37223970e5251569fa03bb24e32153
Nabila <17nabilafahim@gmail.com>
2026-08-01 22:16:36 +0530

    changes on graph

---
+++ Graph.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Graph {
+
+    private Map<Location, List<Road>> adjacencyList;
+
+
+    public Graph(){
+
+        adjacencyList = new HashMap<>();
+
+    }
+
+
+    // Add loc node
+    public void addLocation(Location location){
+
+        adjacencyList.put(location, new ArrayList<>());
+
+    }
+
+
+    // Add edge
+    public void addRoad(Road road){
+
+        Location source = road.getSource();
+
+        adjacencyList.get(source).add(road);
+
+    }
+
+
+    // Get neighbour roads
+    public List<Road> getNeighbours(Location location){
+
+        return adjacencyList.get(location);
+
+    }
+
+
+    // Display graph (for testing)
+    public void displayGraph(){
+
+        for(Location location : adjacencyList.keySet()){
+
+            System.out.println(location + " --> ");
+
+            for(Road road : adjacencyList.get(location)){
+
+                System.out.println(
+                    road.getDestination()
+                );
+            }
+
+        }
+
+    }
+
+}
