6b5b49c05d4a790bac6f885b26a22f0cb3259874
Nabila <17nabilafahim@gmail.com>
2026-08-01 21:50:31 +0530

    added graoh class

---
+++ osm/OSM.java
@@ -0,0 +1,12 @@
+package osm;
+
+public class OSM {
+    public void fetchData() {
+
+        
+        // Connect to Overpass API
+        // Receive JSON
+        // Convert JSON into Location and Road objects
+
+    }
+}
