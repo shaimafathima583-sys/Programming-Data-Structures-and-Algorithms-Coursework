c387ab0cc0ad7dc231820fa5dcfb646f1ffc860c
hirukaushalya301-maker <hirukaushalya301@gmail.com>
2026-08-02 01:33:48 +0530

    Created opportuniy calculator

--- OpportunityCalculator.java
+++ OpportunityCalculator.java
@@ -1,21 +1,38 @@
 public class OpportunityCalculator {
-    public double calculateScore(Location location) {
 
-        // Normalize population (assuming max population = 1,000,000)
-        double populationScore = (location.getPopulation() / 1000000.0) * 100;
 
-        // Convert values to percentages
-        double purchasingPowerScore = location.getPurchasingPower() * 100;
-        double competitionScore = location.getCompetition() * 100;
-        double facilitiesScore = location.getFacilities() * 100;
+    public static double calculateScore(Location location) {
 
-        // Opportunity Score Formula
-        double score =
-                (populationScore * 0.30) +
-                (purchasingPowerScore * 0.35) +
-                (facilitiesScore * 0.25) -
-                (competitionScore * 0.10);
 
-        return Math.round(score * 100.0) / 100.0;
+        double score = 0;
+
+
+        // More population = more customers
+
+        score += location.getPopulation() * 0.00001;
+
+
+
+        // Higher purchasing power is better
+
+        score += location.getPurchasingPower() * 0.5;
+
+
+
+        // More facilities are better
+
+        score += location.getFacilities() * 0.3;
+
+
+
+        // More competition reduces opportunity
+
+        score -= location.getCompetition() * 0.4;
+
+
+
+        return score;
+
     }
+
 }
