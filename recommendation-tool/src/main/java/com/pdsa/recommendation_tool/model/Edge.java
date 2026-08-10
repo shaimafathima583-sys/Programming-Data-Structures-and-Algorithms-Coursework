c0fdbf4152336af02b5978c632f252a388b4fcf3
Shaima <shaimafathima583@gmail.com>
2026-08-10 21:02:22 +0530

    Implemented Edge.java

--- recommendation-tool/src/main/java/com/pdsa/recommendation_tool/model/Edge.java
+++ recommendation-tool/src/main/java/com/pdsa/recommendation_tool/model/Edge.java
@@ -1,5 +1,25 @@
 package com.pdsa.recommendation_tool.model;
 
-public class Edge {
-    
+public class Edge<T> {
+
+    private T destination;
+    private double weight;
+
+    public Edge(T destination, double weight) {
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public T getDestination() {
+        return destination;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return destination + " (" + weight + ")";
+    }
 }
