91167d3fada857cedaf96ceaf474b64f6ccfab7e
Nabila <17nabilafahim@gmail.com>
2026-08-11 13:51:31 +0530

    update geo utils

--- recommendation-tool/src/main/java/com/pdsa/recommendation_tool/osm/GeoUtils.java
+++ recommendation-tool/src/main/java/com/pdsa/recommendation_tool/osm/GeoUtils.java
@@ -12,19 +12,15 @@ public class GeoUtils {
 
         double latDistance = Math.toRadians(lat2 - lat1);
         double lonDistance = Math.toRadians(lon2 - lon1);
+        double radLat1 = Math.toRadians(lat1);
+        double radLat2 = Math.toRadians(lat2);
 
-        double a =
-                Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-                + Math.cos(Math.toRadians(lat1))
-                * Math.cos(Math.toRadians(lat2))
-                * Math.sin(lonDistance / 2)
-                * Math.sin(lonDistance / 2);
 
-        double c = 2 * Math.atan2(
-                Math.sqrt(a),
-                Math.sqrt(1 - a)
-        );
+         double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2)
+                * Math.cos(radLat1) * Math.cos(radLat2);
 
+        double c = 2 * Math.atan2( Math.sqrt(a), Math.sqrt(1 - a));
         return EARTH_RADIUS_KM * c;
     }
 }
